77ce915e37270e58aa9154611f044e406d4da6fb
Juliano C L Santos <foxports@hotmail.com>
2022-02-10 14:01:02 -0300

    Endpoit de usuarios

--- src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resource/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.dicasdeumdev.api.resource;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(id, "Juliano", "juliano@email.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 }
