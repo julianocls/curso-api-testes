ef51a19d5aff0aea2716e02f2e2bd8029ab3482f
Juliano C L Santos <foxports@hotmail.com>
2022-02-14 11:22:11 -0300

    Teste Cenario EndPoint userResource.findAll()

--- src/test/java/br/com/dicasdeumdev/api/resource/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resource/UserResourceTest.java
@@ -11,7 +11,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -38,7 +43,7 @@ class UserResourceTest {
     }
 
     @Test
-    void whenFindByIdSuccess() {
+    void whenFindByIdReturnSuccess() {
         // Scenery
         when(service.findById(anyInt())).thenReturn(user);
         when(mapper.map(any(), any())).thenReturn(userDTO);
@@ -49,6 +54,7 @@ class UserResourceTest {
         // Verifications
         Assertions.assertNotNull(response);
         Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
         Assertions.assertEquals(ResponseEntity.class, response.getClass());
         Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
         Assertions.assertEquals(userDTO.getId(), response.getBody().getId());
@@ -58,7 +64,29 @@ class UserResourceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllReturnListOfUsers() {
+        User user1 = UserBuilder.anUser().withId(1).withNome("User 1").whitEmail("user1@email.com").now();
+        User user2 = UserBuilder.anUser().withId(2).withNome("User 2").whitEmail("user2@email.com").now();
+        User user3 = UserBuilder.anUser().withId(3).withNome("User 3").whitEmail("user3@email.com").now();
+
+        // Scenary
+        when(service.findAll()).thenReturn(Arrays.asList(user1, user2, user3));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        // Action
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        // Verifications
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(ArrayList.class, response.getBody().getClass());
+        Assertions.assertEquals(UserDTO.class, response.getBody().get(0).getClass());
+        Assertions.assertEquals(userDTO.getId(), response.getBody().get(0).getId());
+        Assertions.assertEquals(userDTO.getEmail(), response.getBody().get(0).getEmail());
+        Assertions.assertEquals(userDTO.getNome(), response.getBody().get(0).getNome());
+        Assertions.assertEquals(userDTO.getPassword(), response.getBody().get(0).getPassword());
     }
 
     @Test
