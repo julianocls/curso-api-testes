475d9fcfcf54e5b84b5bbf809769a7a6efde5e27
Juliano C L Santos <foxports@hotmail.com>
2022-02-14 14:35:18 -0300

    Teste Cenario EndPoint userResource.update()

--- src/test/java/br/com/dicasdeumdev/api/resource/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resource/UserResourceTest.java
@@ -4,13 +4,13 @@ import br.com.dicasdeumdev.api.builder.UserBuilder;
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.service.impl.UserServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -18,10 +18,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class UserResourceTest {
 
     @InjectMocks
@@ -52,15 +54,15 @@ class UserResourceTest {
         ResponseEntity<UserDTO> response = resource.findById(anyInt());
 
         // Verifications
-        Assertions.assertNotNull(response);
-        Assertions.assertNotNull(response.getBody());
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-        Assertions.assertEquals(ResponseEntity.class, response.getClass());
-        Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
-        Assertions.assertEquals(userDTO.getId(), response.getBody().getId());
-        Assertions.assertEquals(userDTO.getEmail(), response.getBody().getEmail());
-        Assertions.assertEquals(userDTO.getNome(), response.getBody().getNome());
-        Assertions.assertEquals(userDTO.getPassword(), response.getBody().getPassword());
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+        assertEquals(userDTO.getId(), response.getBody().getId());
+        assertEquals(userDTO.getEmail(), response.getBody().getEmail());
+        assertEquals(userDTO.getNome(), response.getBody().getNome());
+        assertEquals(userDTO.getPassword(), response.getBody().getPassword());
     }
 
     @Test
@@ -77,24 +79,49 @@ class UserResourceTest {
         ResponseEntity<List<UserDTO>> response = resource.findAll();
 
         // Verifications
-        Assertions.assertNotNull(response);
-        Assertions.assertNotNull(response.getBody());
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-        Assertions.assertEquals(ResponseEntity.class, response.getClass());
-        Assertions.assertEquals(ArrayList.class, response.getBody().getClass());
-        Assertions.assertEquals(UserDTO.class, response.getBody().get(0).getClass());
-        Assertions.assertEquals(userDTO.getId(), response.getBody().get(0).getId());
-        Assertions.assertEquals(userDTO.getEmail(), response.getBody().get(0).getEmail());
-        Assertions.assertEquals(userDTO.getNome(), response.getBody().get(0).getNome());
-        Assertions.assertEquals(userDTO.getPassword(), response.getBody().get(0).getPassword());
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(0).getClass());
+        assertEquals(userDTO.getId(), response.getBody().get(0).getId());
+        assertEquals(userDTO.getEmail(), response.getBody().get(0).getEmail());
+        assertEquals(userDTO.getNome(), response.getBody().get(0).getNome());
+        assertEquals(userDTO.getPassword(), response.getBody().get(0).getPassword());
     }
 
     @Test
-    void save() {
+    void whenCreateReturnsCreated() {
+        // Scenery
+        when(service.create(any())).thenReturn(user);
+
+        // Action
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        // Verifications
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
-    void update() {
+    void whenUpdateReturnUserUpdated() {
+        // Scenery
+        when(service.update(any())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        // Action
+        ResponseEntity<UserDTO> response = resource.update(userDTO.getId(), userDTO);
+
+        // Verifications
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+        assertEquals(userDTO.getId(), response.getBody().getId());
+        assertEquals(userDTO.getEmail(), response.getBody().getEmail());
+        assertEquals(userDTO.getNome(), response.getBody().getNome());
+        assertEquals(userDTO.getPassword(), response.getBody().getPassword());
     }
 
     @Test
