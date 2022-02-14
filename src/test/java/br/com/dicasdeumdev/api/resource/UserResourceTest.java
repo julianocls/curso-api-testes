dcb33dd98d5ac0e95f0e813f18b2688275e27d17
Juliano C L Santos <foxports@hotmail.com>
2022-02-14 11:00:42 -0300

    Teste Cenario EndPoint userResource.findById

--- src/test/java/br/com/dicasdeumdev/api/resource/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resource/UserResourceTest.java
@@ -3,19 +3,19 @@ package br.com.dicasdeumdev.api.resource;
 import br.com.dicasdeumdev.api.builder.UserBuilder;
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
-import br.com.dicasdeumdev.api.repository.UserRepository;
-import br.com.dicasdeumdev.api.service.UserService;
 import br.com.dicasdeumdev.api.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 class UserResourceTest {
 
@@ -38,7 +38,23 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdSuccess() {
+        // Scenery
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        // Action
+        ResponseEntity<UserDTO> response = resource.findById(anyInt());
+
+        // Verifications
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
+        Assertions.assertEquals(userDTO.getId(), response.getBody().getId());
+        Assertions.assertEquals(userDTO.getEmail(), response.getBody().getEmail());
+        Assertions.assertEquals(userDTO.getNome(), response.getBody().getNome());
+        Assertions.assertEquals(userDTO.getPassword(), response.getBody().getPassword());
     }
 
     @Test
